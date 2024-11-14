84e83c9e643cb1e6ab25c493458daae5a01536cb
Temidire Damian <vladimirwayne9@gmail.com>
2024-11-14 00:55:50 +0000

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./components/**/*.{html,js}",
+    "./assets/js/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
